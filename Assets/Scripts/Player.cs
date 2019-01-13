9dfaf852af071ddb06353b51086a54d3b3bf9abc
TomDevaney <asianman547@gmail.com>
2019-01-13 15:02:17 -0800

    Player moves left and right

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (InputManager.instance.CanPlayerMove())
+        {
+            float xAxis = Input.GetAxis("Horizontal");
+
+            transform.Translate(new Vector2(xAxis * 5.0f, 0.0f));
+        }
+    }
+}
