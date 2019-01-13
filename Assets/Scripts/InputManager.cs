247d7c31ff216bb895105dec236121a12cf06bd2
TomDevaney <asianman547@gmail.com>
2019-01-13 15:01:43 -0800

    Set up input manager.
    It limits what the player can do, and it does events for input

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    // Singleton so classes can reference without reference
+    public static InputManager instance = null;
+
+    // I am just using a delegate event to save some speed. I don't need this in editor as of right now
+    // If I wanted in editor, change this to UnityEvent to sacrifice some speed
+    // Event for left mouse released
+    public delegate void LeftMouseReleased();
+    public static event LeftMouseReleased OnLeftMouseReleased;
+
+    // 
+    bool canPlayerMove;
+
+    //
+    bool canPlayerJump;
+
+    //
+    bool canPlayerAttack;
+
+    // Awake is called before Start
+    void Awake()
+    {
+        // Set up singleton
+        if (!instance)
+        {
+            instance = this;
+        }
+
+        //Sets this to not be destroyed when reloading scene... Not sure it's needed for this game
+        //DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        // Initialize variables
+        canPlayerMove = true;
+        canPlayerJump = true;
+        canPlayerAttack = true;
+    }
+
+    void Update()
+    {
+        // Handle left mouse released event
+        if (OnLeftMouseReleased != null)
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                OnLeftMouseReleased();
+            }
+        }
+    }
+
+    /* Getters */
+    public bool CanPlayerMove()
+    {
+        return canPlayerMove;
+    }
+
+    public bool CanPlayerJump()
+    {
+        return canPlayerJump;
+    }
+
+    public bool CanPlayerAttack()
+    {
+        return canPlayerAttack;
+    }
+
+    /* Setters */
+    public void SetCanPlayerMove(bool canMove)
+    {
+        canPlayerMove = canMove;
+    }
+}
