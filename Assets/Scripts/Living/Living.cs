d478dc4f0a70332de458d81332cad0a074c7efb9
TomDevaney <asianman547@gmail.com>
2019-01-14 21:21:18 -0800

    Created a living base class.
    Enemy and Puppet inherit from it.

---
+++ Assets/Scripts/Living/Living.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Living : MonoBehaviour
+{
+    // Their health
+    int healthPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int damage)
+    {
+        // Apply damage
+        healthPoints -= damage;
+
+        // Check if dead
+        if (healthPoints <= 0)
+        {
+            JustDied();
+        }
+    }
+
+    // Override in the child and react to the death
+    public virtual void JustDied()
+    {
+
+    }
+}
