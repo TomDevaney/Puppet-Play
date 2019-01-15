0f2bb699d3f6810e496b32f9655d3c388790e003
AndyHollist <aholl006@ucr.edu>
2019-01-14 23:12:34 -0800

    New Player Tag added, jump functions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,21 +4,73 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+	public float MoveRate = 0.01f;
+
+	public Rigidbody TheRigidBody;
+
+	public CapsuleCollider Capsule;
+
+    public bool IsStandingOnSurface = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+		TheRigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        int LayerMask = 1 << 9;
+        LayerMask = ~LayerMask;
+        float MaxRayDistance = 0.2f;
+        RaycastHit HitInfo;
+		if(Physics.Raycast(transform.position, transform.up * -1, out HitInfo, MaxRayDistance, LayerMask))
+        {
+            IsStandingOnSurface = true;
+            print(HitInfo.distance + "");
+        }
+        else
+        {
+            IsStandingOnSurface = false;
+        }
+
+
         if (InputManager.instance.CanPlayerMove())
         {
+			//A Left D Right
             float xAxis = Input.GetAxis("Horizontal");
 
             //transform.Translate(new Vector2(xAxis * 5.0f, 0.0f));
-            transform.Translate(new Vector2(xAxis * 0.01f, 0.0f));
+			transform.Translate(new Vector2(xAxis * MoveRate, 0.0f));
         }
-    }
+
+		if(InputManager.instance.CanPlayerJump() && IsStandingOnSurface)
+		{
+			if(Input.GetButtonUp("Jump"))
+			{
+				Jump(150);
+			}
+		}
+
+
+	}
+
+	public void OnCollisionEnter(Collision Col)
+	{
+		//print("OnCollisionEnter");
+
+	}
+
+	public void OnCollisionExit()
+	{
+		//print("OnCollisionExit");
+	}
+
+	public void Jump(float force)
+	{
+		//print("Jumped");
+
+		TheRigidBody.AddForce( 0, force , 0);
+	}
 }
