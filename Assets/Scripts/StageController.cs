5afde2a196889ee9f83f753616c211c8123ead0c
AndyHollist <aholl006@ucr.edu>
2019-01-17 02:47:18 -0800

    Added StageController.cs

---
+++ Assets/Scripts/StageController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageController : MonoBehaviour
+{
+    public GameObject LeftCurtain = null;
+    public GameObject RightCurtain = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LeftCurtain = GameObject.Find("CurtainLeft");
+        RightCurtain = GameObject.Find("CurtainRight");
+
+        OpenCurtains();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OpenCurtains()
+    {
+        print("OpenCurtains");
+        if(LeftCurtain == null)
+        {
+            print("null");
+        }
+
+    }
+
+    IEnumerator OpeningCurtains()
+    {
+
+        yield return new WaitForSeconds(.01f);
+    }
+
+    public void CloseCurtains()
+    {
+
+    }
+}
