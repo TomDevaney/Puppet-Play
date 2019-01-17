12dfc9142c5827b15465795f48fece3ef54f2333
AndyHollist <aholl006@ucr.edu>
2019-01-16 18:23:27 -0800

    Follow Camera implemented

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
+
+    public Puppet PlayerPuppet;
+
+    public float LevelBoundLeft;
+
+    public float LevelBoundRight;
+
+    public float TargetY;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerPuppet = GameObject.Find("Player").GetComponent<Puppet>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        GameObject.Find("Player");
+        float nextX = LevelBoundLeft;
+
+
+        nextX = Mathf.Lerp(transform.position.x, PlayerPuppet.transform.position.x, Time.deltaTime);
+        transform.position = new Vector3(nextX, TargetY, transform.position.z);
+    }
+
+    public void OnRespawn()
+    {
+        PlayerPuppet = GameObject.Find("Player").GetComponent<Puppet>();
+        transform.position = new Vector3(PlayerPuppet.transform.position.x, TargetY, transform.position.z);
     }
 }
