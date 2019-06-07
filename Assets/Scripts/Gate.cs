72fddd13bca4043e34b7420c8ae7b97789834048
TomDevaney <asianman547@gmail.com>
2019-06-07 02:37:00 -0700

    Got new model for gate in, and it now rotates open

--- Assets/Scripts/Gate.cs
+++ Assets/Scripts/Gate.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Gate : MonoBehaviour
 {
+	public float openToDegrees;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,12 +20,35 @@ public class Gate : MonoBehaviour
 
 	public void Open()
 	{
-		// TODO: Play animation
+		// Play animation
+		StartCoroutine("OpenGate");
 
 		// TODO: Play sound
 
-		// Temp solution
-		Destroy(gameObject);
+		// Disable collision so it doesn't move the player when opening
+		GetComponentInChildren<BoxCollider>().enabled = false;
+	}
+
+	IEnumerator OpenGate()
+	{
+		float TimeBetween = 0.0035f;
+		float ratio = 0.0f;
+
+		while (true)
+		{
+			float nextY = Mathf.Lerp(0.0f, openToDegrees, ratio);
+
+			transform.rotation = Quaternion.Euler(transform.rotation.x, nextY, transform.rotation.z);
+
+			if (nextY == openToDegrees)
+			{
+				break;
+			}
+
+			ratio += TimeBetween;
+
+			yield return new WaitForSeconds(TimeBetween);
+		}
 
 		EventManager.instance.MarkEventAsDone();
 	}
